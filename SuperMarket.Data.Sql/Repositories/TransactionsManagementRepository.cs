4cd6481f8338ef69533860dcd61a0a8ccdc1e4fb
Mouad <Mag@DESKTOP-7QDCA67>
2022-02-13 18:19:03 +0100

    Ajoutez des fichiers projet.

---
+++ SuperMarket.Data.Sql/Repositories/TransactionsManagementRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperMarket.Core;
+using SuperMarket.Management.ManagementInterafaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Data.Sql.Repositories
+{
+    public class TransactionsManagementRepository : ITransactionsManagementRepository
+    {
+        private readonly AppDbContext db;
+
+        public TransactionsManagementRepository(AppDbContext _db)
+        {
+            db = _db;
+        }
+
+        public IEnumerable<Transa> Get(string cashierName)
+        {
+            return db.Transactions.ToList();
+        }
+
+        public IEnumerable<Transa> GetByDay(string cashierName, DateTime date)
+        {
+            if (string.IsNullOrWhiteSpace(cashierName))
+                return db.Transactions.Where(x => x.TimeStamp.Date == date.Date);
+            else
+                return db.Transactions.Where(x =>
+                    EF.Functions.Like(x.CashierName, $"%{cashierName}%") &&
+                    x.TimeStamp.Date == date.Date);
+        }
+
+        public void Save(string cashierName, int productId, string productName, double price, int beforeQty, int soldQty)
+        {
+            var transaction = new Transa
+            {
+                ProductId = productId,
+                ProductName = productName,
+                TimeStamp = DateTime.Now,
+                Price = price,
+                BeforeQty = beforeQty,
+                SoldQty = soldQty,
+                CashierName = cashierName
+            };
+
+            db.Transactions.Add(transaction);
+            db.SaveChanges();
+        }
+
+        public IEnumerable<Transa> Search(string cashierName, DateTime startDate, DateTime endDate)
+        {
+            if (string.IsNullOrWhiteSpace(cashierName))
+                return db.Transactions.Where(x => x.TimeStamp >= startDate.Date && x.TimeStamp <= endDate.Date.AddDays(1).Date);
+            else
+                return db.Transactions.Where(x =>
+                    EF.Functions.Like(x.CashierName, $"%{cashierName}%") &&
+                    x.TimeStamp >= startDate.Date && x.TimeStamp <= endDate.Date.AddDays(1).Date);
+        }
+    }
+}
---
+++ SuperMarket.Management/ProductManagement/SellProduct.cs
@@ -0,0 +1,35 @@
+﻿using SuperMarket.Data.InMemory;
+using SuperMarket.Management.ProductInterface;
+using SuperMarket.Management.TranscationsInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Management.ProductManagement
+{
+    public class SellProduct : ISellProduct
+    {
+        private readonly IProductManagementRepository productInMemory;
+        private readonly IRecordTransactions recordTransaction;
+
+        public SellProduct(
+            IProductManagementRepository _productInMemory
+            , IRecordTransactions _recordTransaction)
+        {
+            productInMemory = _productInMemory;
+            recordTransaction = _recordTransaction;
+        }
+
+        public void Execute(string cashierName, int productId, int qtyToSell)
+        {
+            var product = productInMemory.GetProductById(productId);
+            if (product == null) return;
+            recordTransaction.Execute(cashierName, productId, qtyToSell);
+            //Supprimer la quantité vendu de la quantité totale
+            product.Quantity -= qtyToSell;
+            productInMemory.UpdateProduct(product);
+        }
+    }
+}
